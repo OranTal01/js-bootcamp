f195a5e6719bc63a9c9270375b784ce18077c1e8
oran <orantal01@gmail.com>
2020-02-13 21:14:55 +0200

    first commit

--- hangman/src/requests.js
+++ hangman/src/requests.js
@@ -1,20 +1,21 @@
-const getPuzzle = async(wordCount) => {
+const getPuzzle = async (wordCount) => {
     const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
     if (response.status === 200) {
         const data = await response.json()
+        console.log(data.puzzle)
         return data.puzzle
     } else {
         throw new Error('Unable to get puzzle')
     }
 }
 
-const getCurrentCountry = async() => {
+const getCurrentCountry = async () => {
     const location = await getLocation()
     return getCountry(location.country)
 }
 
-const getCountry = async(countryCode) => {
+const getCountry = async (countryCode) => {
     const response = await fetch('http://restcountries.eu/rest/v2/all')
 
     if (response.status === 200) {
@@ -25,7 +26,7 @@ const getCountry = async(countryCode) => {
     }
 }
 
-const getLocation = async() => {
+const getLocation = async () => {
     const response = await fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c')
 
     if (response.status === 200) {
